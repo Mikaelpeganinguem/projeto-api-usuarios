f8ff6f9e44bfb0ede32c0aeec5b883b42d9b32f9
Mikael Carlos <carlosmikael273@gmail.com>
2025-01-03 08:30:03 -0300

    Simplificação do código no arquivo dbUser.js

--- routes/database/users/dbUser.js
+++ routes/database/users/dbUser.js
@@ -2,7 +2,6 @@ const fs = require('fs');
 const path = require('path');
 
 const datafile = path.join(__dirname, 'users.json');
-const DATABASE = 'users.json';  
 
 function readFile() {
     if (!fs.existsSync(datafile)) {
@@ -10,21 +9,17 @@ function readFile() {
     }
 
     const data = fs.readFileSync(datafile, 'utf-8');
-    
-    try {
-        return JSON.parse(data);
-    } catch (error) {
-        console.error("Erro ao parsear JSON:", error);
-        return [];
-    }
+    return JSON.parse(data);
+    // try {
+    //     return JSON.parse(data);
+    // } catch (error) {
+    //     console.error("Erro ao parsear JSON:", error);
+    //     return [];
+    // }
 }
 
 function saveData(data) {
-    if (Array.isArray(data)) {
-        fs.writeFileSync(datafile, JSON.stringify(data, null, 2), 'utf-8');
-    } else {
-        console.error("Erro: Dados não são um array");
-    }
+    fs.writeFileSync(datafile, JSON.stringify(data, null, 2), 'utf-8');    
 }
 
 module.exports = { readFile, saveData };
